deb7b3ff01e1c140895206f5c120d69eabb0bcc9
tenko <tajm.ogunbiyi@gmail.com>
2022-07-26 17:17:26 +0100

    :construction_worker: IFakeNFTMarketplace interface written

--- hardhat/contracts/CryptoDevsDAO.sol
+++ hardhat/contracts/CryptoDevsDAO.sol
@@ -3,6 +3,15 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-interface IFakeNFTMarketplace {}
+interface IFakeNFTMarketplace {
+    // returns the price in Wei of and nft from FakeNFTMarketplace
+    function getPrice() external view returns (uint);
+
+    // returns whether or not given tokenID has already been purchsed
+    function available(uint256 _tokenId) external view returns (bool);
+
+    // purchases nft of tokenID from marketplace
+    function purchase(uint256 _tokenId) external payable;
+}
 
 contract CryptoDevsDAO is Ownable {}
